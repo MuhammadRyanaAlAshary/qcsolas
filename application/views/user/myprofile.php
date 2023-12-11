d1f2e2451fb820848006226193d8bbc7e6cebc34
valentioaditama <valentioaditama8@gmail.com>
2023-12-11 14:38:21 +0700

    update db

--- application/views/user/myprofile.php
+++ application/views/user/myprofile.php
@@ -7,13 +7,13 @@
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-fluid rounded-start" alt="...">
+                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="img-fluid rounded-start p-3" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title"><?= $user['name']; ?></h5>
                     <p class="card-text"><?= $user['email']; ?></p>
-                    <p class="card-text"><small class="text-body-secondary">Member Since <?= date('d F Y', $user['date_created']); ?></small>
+                    <p class="card-text"><small class="text-body-secondary">Member Since <?= $user['date_created']; ?></small>
                     </p>
                 </div>
             </div>
